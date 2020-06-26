0f110c1d34cddb3c0d024137094baf37db2fd352
Rithika-Raj <rithuraj08@gmail.com>
2020-06-26 17:02:01 +0530

    added files

---
+++ src/environments/environment.ts
@@ -0,0 +1,45 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDtGBhQAxsq8szRRZyCHWwmuJX0yspKAgc",
+    authDomain: "ideapool-be85a.firebaseapp.com",
+    databaseURL: "https://ideapool-be85a.firebaseio.com",
+    projectId: "ideapool-be85a",
+    storageBucket: "ideapool-be85a.appspot.com",
+    messagingSenderId: "60901228099",
+    appId: "1:60901228099:web:e173aebcc9faaeecf9def6",
+    measurementId: "G-ZT1NWD0XHF"
+  }
+  };
+
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+//<!-- The core Firebase JS SDK is always required and must be listed first -->
+//<script src="/__/firebase/7.15.5/firebase-app.js"></script>
+
+//<!-- TODO: Add SDKs for Firebase products that you want to use
+//     https://firebase.google.com/docs/web/setup#available-libraries -->
+//<script src="/__/firebase/7.15.5/firebase-analytics.js"></script>
+
+//<!-- Initialize Firebase -->
+//<script src="/__/firebase/init.js"></script>
+
+//npm install -g firebase-tools
+//firebase login
+//firebase init
+//firebase deploy
+
+//After deploying, view your app at ideapool-be85a.web.app
